8392ef3db414d5ed069b5cb0a59037553680aa5f
danmaxgit <danmax11@yandex.ru>
2024-06-02 16:09:10 +0300

    added HashMap class

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "time.h"
+#include <complex.h>
 #include <stdbool.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -29,6 +29,74 @@ bool validate_input(char *input) {
     return false;
 }
 
+
+
+
+typedef struct Node{
+    char value[7];
+    int num;
+    struct Node* next;
+} Node;
+
+typedef struct HashMap {
+    Node* data;
+    int (*hash)(char*);
+    int size;
+} HashMap;
+
+HashMap* init_hash(int len, int (*h)(char*)){
+    HashMap* m = (HashMap*)malloc(sizeof(HashMap));
+    m->data = (Node*)malloc(sizeof(Node)*len);
+    for (int i = 0; i < len; ++i) {
+        m->data[i].next = NULL;
+    }
+    m->hash = h;
+    m->size = len;
+    return m;
+}
+void free_hash(HashMap *m){
+    Node* data[10000];
+    int size = 0;
+    for (int i = 0; i < m->size; i++) {
+        if (m->data[i].next) {
+            Node* t = m->data[i].next;
+            data[size] = t;
+            size++;
+            while (t->next) {
+                t = t->next;
+                data[size] = t;
+                size++;
+            }
+            for (int i = 0; i < size; i++) {
+                free(data[i]);
+            }
+        }
+        size = 0;
+    }
+    free(m->data);
+    free(m);
+}
+void append_hash(HashMap *m, char *v){
+    if (m->data[m->hash(v)].num == 0) {
+        for (int i = 0; i < 7; i++) {
+            m->data[m->hash(v)].value[i] = v[i];
+        }
+        m->data[m->hash(v)].num++;
+    }
+    else {
+        Node* t = &m->data[m->hash(v)];
+        m->data[m->hash(v)].num++;
+        while (t->next) {
+            t = t->next;
+        }
+        t->next = (Node *)malloc(sizeof(Node));
+         for (int i = 0; i < 7; i++) {
+            t->next->value[i] = v[i];
+        }
+    }   
+}
+
+
 int main(int argc, char** argv){
     srand(time(NULL));
     char buff[100];
@@ -36,11 +104,10 @@ int main(int argc, char** argv){
     //    scanf("%s", buff);
     //} while (!validate_input(buff));
     char temp;
-    int* map = (int*)malloc(sizeof(int) * 1464);
-    for (int i = 0; i < 1464; i++) {
-        map[i] = 0;
-    }
-    for (int i = 0; i < 10000; ++i) {
+
+    HashMap *map = init_hash(1464, hash);
+
+    for (int i = 0; i < 4; ++i) {
         for (int j = 0; j < 6; ++j) {
             switch (j) {
                 case 0:
@@ -57,16 +124,12 @@ int main(int argc, char** argv){
             buff[j] = temp;
         }
         buff[7] = '\0';
-        // printf("%s\n", buff);
-        int h = hash(buff);
-        map[h]++;
+        append_hash(map, buff);
     }
     FILE *f = fopen("/home/danmax/code/saod/l4/data.csv", "w");
     for (int i = 0; i < 1464; i++) {
-        fprintf(f, "%d,%d\n", i, map[i]);
-    }
-
-    fclose(f);
-    free(map);
+         fprintf(f, "%d,%d\n", i, map->data[i].num);
+        }
+    free_hash(map);
     return 0;   
 }
